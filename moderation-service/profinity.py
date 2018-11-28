3e112e4dfa4aae7fb9548253c994ea6618095f86
root <root@ip-172-31-18-53.us-west-1.compute.internal>
2018-11-28 01:34:00 +0000

    Add moderation service

---
+++ moderation-service/profinity.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify, abort
+import logging
+import boto3
+import json
+
+app = Flask(__name__)
+from profanityfilter import ProfanityFilter
+pf = ProfanityFilter()
+
+
+@app.route('/get_profinity', methods=['POST'])
+def getText():
+    print "request is ", request.data
+    data = request.get_json()
+    print("data i got is ", data)
+    text = data['text']
+    profined  = pf.censor(text)
+    counter = 0
+    code = "AllOK"
+    for words in profined.split(' '):
+        print words
+        if '*' in words:
+            print "found it"
+            counter = counter + 1
+       
+        if 'Marry' in words:
+            print "found Marryi"
+            code =  "M400"
+    if counter >2:
+       code ="P101"
+    if counter >2 or code == "M400":
+       send_sns(data)
+       profined = "Our content moderation has flagged you message. Your message won't be delivered"
+    retdict = {}
+    retdict['text'] = profined
+    retdict['count'] = counter
+    retdict['code'] = code
+    return jsonify(retdict)
+
+def send_sns(message):
+    client = boto3.client('sns', region_name="us-west-1")
+    arn = "arn:aws:sns:us-west-1:835733446678:profinity"
+    response = client.publish(
+                   TopicArn = arn,
+                       Message=json.dumps({'default': json.dumps(message)}),
+                           MessageStructure='json'                        )
+
+if __name__ == "__main__":
+    LOG_FORMAT = '[%(asctime)s] %(process)d %(module)-12s %(levelname)-8s %(message)s'
+    DATE_FORMAT = '%d/%b/%Y %H:%M:%S %z'
+    logging.basicConfig(level=logging.INFO,
+                        format=LOG_FORMAT, datefmt=DATE_FORMAT)
+    app.run(host="0.0.0.0", port=7080, threaded=True)
+
